34520178f9020ed59ec3e0152064a456af6d2ac1
Broderick Carlin <broderick.carlin@gmail.com>
2024-12-12 23:18:16 -0600

    feat: Add USB serialport support to the template

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,27 @@ use embassy_rp::gpio::{Level, Output};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+mod usb_serial;
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    // NOTE: The logs will only show if using a debug probe
+    let mut sp = usb_serial::init(&spawner, p.USB);
+
+    // NOTE: info!() only goes to a debug probe, sp.print() goes out USB
     info!("Hello World!");
+    sp.print("Hello World!\n").await;
 
     let mut led = Output::new(p.PIN_25, Level::Low);
 
     loop {
         info!("high");
+        sp.print("high\n").await;
         led.set_high();
         Timer::after_millis(1000).await;
 
         info!("low");
+        sp.print("low\n").await;
         led.set_low();
         Timer::after_millis(1000).await;
     }
